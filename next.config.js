b936bdb974415231ce1e5a8e2a60e0d0ae67822c
김태우 <twkim@k-auction.com>
2025-03-26 14:11:12 +0900

    fix: Add webpack configuration for Tailwind CSS

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "export",
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ["style-loader", "css-loader", "postcss-loader"],
+    });
+    return config;
   },
 };
 
